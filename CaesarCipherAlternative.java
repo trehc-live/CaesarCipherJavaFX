0b38e2ab4b6e5dc1fdc8fc03d1aa7ae06d10f7e1
trehc-live <125083188+trehc-live@users.noreply.github.com>
2024-10-14 12:59:22 +0300

    Added Alternative Caesar cipher algorithm

---
+++ CaesarCipherAlternative.java
@@ -0,0 +1,13 @@
+package com.stackoverflow.q3732109.aesarcipherjavafx.services;
+
+public class CaesarCipherAlternative {
+
+    public static String encrypt(String text, int key){
+        char[] encrypted = text.toCharArray();
+        for (int i = 0; i < text.length(); i++) {
+            char temp = encrypted[i];
+            encrypted[i] = (char)(encrypted[i] + key);
+        }
+        return new String(encrypted);
+    }
+}
